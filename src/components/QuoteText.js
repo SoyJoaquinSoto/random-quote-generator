70e0491a9b40ba1ebde3da200721603c7d271b54
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-16 17:23:03 -0600

    Added styling to QuoteText

--- src/components/QuoteText.js
+++ src/components/QuoteText.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function QuoteText() {
     return (
-        <h1>Text</h1>
+        <h1 className={`font-display text-white text-7xl`}>Text</h1>
     );
 }
