002c7a4bb1baad8722f9b13d41d8b3ee25e84b52
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-16 17:10:07 -0600

    Added QuoteTable body
    Added QuoteText and QuoteAuthor

--- src/components/QuoteTable.js
+++ src/components/QuoteTable.js
@@ -1,9 +1,14 @@
 import React from "react"
+import QuoteText from "./QuoteText.js"
+import QuoteAuthor from "./QuoteAuthor.js"
 
 export default class QuoteTable extends React.Component {
     render() {
         return (
-            <div className={`bg-red-100 h-full w-full`}></div>
+            <div className={`bg-red-100 h-5/6 w-5/6 m-auto`}>
+                <QuoteText />
+                <QuoteAuthor />
+            </div>
         );
     }
 }
---
+++ src/components/QuoteText.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function QuoteText() {
+    return (
+        <h1>Text</h1>
+    );
+}
