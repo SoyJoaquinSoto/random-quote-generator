e3892b05a287ebb3d1fdb7e79a0a94acc5285473
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-16 16:48:25 -0600

    Added a random color utility
    When selecting the background color of the App the random color utility is used

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default class App extends React.Component {
+
+    randomColor() {
+        const colors = [
+            "gray",
+            "red",
+            "yellow",
+            "green",
+            "blue",
+            "indigo",
+            "purple",
+            "pink",
+        ]
+        return colors[Math.floor(Math.random() * 8)];
+    }
+
+    render() {
+        return (
+            <div className={`bg-${this.randomColor()}-500 h-screen w-screen`}>
+                
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
+import App from "./App.js"
 
 ReactDOM.render (
-    <div></div>,
+    <App />,
     document.getElementById('root')
 );
