2f71cfab4ba20e2d8666e413181d1c83cb5bd8da
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-16 17:24:00 -0600

    Added styling to QuoteAuthor

--- src/components/QuoteAuthor.js
+++ src/components/QuoteAuthor.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function QuoteAuthor() {
     return (
-        <h1>Author</h1>
+        <h1 className={`font-display text-white text-2xl`}>Author</h1>
     );
 }
