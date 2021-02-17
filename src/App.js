ab826dc412414f2f35a849ccf1047c3f8823127c
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-16 22:26:57 -0600

    Lifted consumption from QuoteTable to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import QuoteTable from "./components/QuoteTable.js";
 import PullButton from "./components/PullButton.js";
 
 export default function App() {
 	const [color, setColor] = useState(null);
+	const [data, setData] = useState({
+		quoteText: null,
+		quoteAuthor: null,
+		quoteGenre: null,
+	});
 
 	useEffect(() => {
 		setColor(randomColor());
+
+		async function fetchData() {
+			const request = await axios.get(
+				"https://quote-garden.herokuapp.com/api/v3/quotes/random"
+			);
+			const quoteData = request.data.data[0];
+			if (!quoteData) {
+				fetchData();
+				return;
+			}
+			if (quoteData.quoteText.length > 110) {
+				fetchData();
+			} else {
+				setData(quoteData);
+				console.log(quoteData);
+			}
+		}
+		fetchData();
 	}, []);
 
 	const randomColor = () => {
@@ -32,7 +56,7 @@ export default function App() {
 		<div
 			className={`bg-${color}-500 h-screen w-screen grid grid-cols-1 lg:grid-cols-2`}
 		>
-			<QuoteTable />
+			<QuoteTable data={data} />
 			<PullButton onClick={() => handleClick()} />
 		</div>
 	);
--- src/components/QuoteTable.js
+++ src/components/QuoteTable.js
@@ -4,35 +4,12 @@ import QuoteText from "./QuoteText.js";
 import QuoteAuthor from "./QuoteAuthor.js";
 import QuoteGenre from "./QuoteGenre.js";
 
-export default function QuoteTable() {
-	const [text, setText] = useState(null);
-	const [author, setAuthor] = useState(null);
-	const [genre, setGenre] = useState(null);
-
-	useEffect(() => {
-		async function fetchData() {
-			var request;
-			var quoteData;
-			request = await axios.get(
-				"https://quote-garden.herokuapp.com/api/v3/quotes/random"
-			);
-			quoteData = request.data.data[0];
-			if (quoteData.quoteText.length > 110) {
-				fetchData();
-			} else {
-				setText(quoteData.quoteText);
-				setAuthor(quoteData.quoteAuthor);
-				setGenre(quoteData.quoteGenre);
-			}
-		}
-		fetchData();
-	}, []);
-
+export default function QuoteTable(props) {
 	return (
 		<div className={`h-auto w-5/6 m-auto`}>
-			<QuoteText text={text} />
-			<QuoteAuthor author={author} />
-			<QuoteGenre genre={genre} />
+			<QuoteText text={props.data.quoteText} />
+			<QuoteAuthor author={props.data.quoteAuthor} />
+			<QuoteGenre genre={props.data.quoteGenre} />
 		</div>
 	);
 }
