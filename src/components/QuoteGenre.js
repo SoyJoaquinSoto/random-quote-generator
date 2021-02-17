3e47fefec9d1ba91e0ab09a828f6baadbf22b5f4
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-16 19:42:06 -0600

    Added a QuoteGenre component

---
+++ src/components/QuoteGenre.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function QuoteGenre(props) {
+	return (
+		<div className={``}>
+			<h1 className={`font-display text-white text-1xl`}>{props.genre}</h1>
+		</div>
+	);
+}
--- src/components/QuoteTable.js
+++ src/components/QuoteTable.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import QuoteText from "./QuoteText.js";
 import QuoteAuthor from "./QuoteAuthor.js";
+import QuoteGenre from "./QuoteGenre.js";
 
 export default function QuoteTable() {
 	const [text, setText] = useState(null);
 	const [author, setAuthor] = useState(null);
+	const [genre, setGenre] = useState(null);
 
 	useEffect(() => {
 		async function fetchData() {
@@ -21,6 +23,7 @@ export default function QuoteTable() {
 			} else {
 				setText(quoteData.quoteText);
 				setAuthor(quoteData.quoteAuthor);
+				setGenre(quoteData.quoteGenre);
 			}
 		}
 		fetchData();
@@ -30,6 +33,7 @@ export default function QuoteTable() {
 		<div className={`h-auto w-5/6 m-auto`}>
 			<QuoteText text={text} />
 			<QuoteAuthor author={author} />
+			<QuoteGenre genre={genre} />
 		</div>
 	);
 }
