708c5db466ac2fbeb2e33786ca7be183453f9e74
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-02-17 23:53:47 -0600

    Added classes to the tailwind safe list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-    fontFamily: {
-      'display': ['Bebas\\ Neue']
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: {
+		content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+		options: {
+			safelist: [
+				"bg-gray-500",
+				"bg-red-500",
+				"bg-yellow-500",
+				"bg-green-500",
+				"bg-blue-500",
+				"bg-indigo-500",
+				"bg-purple-500",
+				"bg-pink-500",
+			],
+		},
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		fontFamily: {
+			display: ["Bebas\\ Neue"],
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
